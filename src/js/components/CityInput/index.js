1f25b6fcc4245dadb3f90a1fb1241b6eb7f521a5
Asten Fuller <astenhfuller@gmail.com>
2020-01-09 12:36:24 -0800

    Spacing

--- src/js/components/CityInput/index.js
+++ src/js/components/CityInput/index.js
@@ -1,8 +1,6 @@
 import { connect } from 'react-redux';
 import CityInput from './CityInput';
 
-    // This function takes the store and returns an object
-    // that's passed to the props of the component.
     function mapStoreToProps(store) {
         return {
             city: store.city,
--- src/js/components/WeatherInfo/index.js
+++ src/js/components/WeatherInfo/index.js
@@ -1,8 +1,6 @@
 import { connect } from 'react-redux';
 import WeatherInfo from './WeatherInfo';
 
-    // This function takes the store and returns an object
-    // that's passed to the props of the component.
     function mapStoreToProps(store) {
         return {
             city: store.city,
